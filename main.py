4e64d9e5f33d9f8a0b2d68764abcb799d32b6d22
Sethu131 <123773376+Sethu131@users.noreply.github.com>
2023-01-27 22:33:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-for i in range(1, 11):
-    if i == 6:
-        continue
-        print(i, end=',')
-    else:
-        print(i, end=',')
+from datetime import date, timedelta
+
+
+def compute_prev_date(dates_list: list):
+    """
+    function will take a list of dates in this format: YYYY-MM-DD
+    and for each date returns previous date in the format DD xxx YYYY
+    where the xxx is the month as locale's abbreviation
+    """
+
+    return [
+        (date.fromisoformat(date_str) - timedelta(days=1)).strftime("%d %b %y")
+        for date_str in dates_list
+    ]
+
+
+list_of_dates = ["2022-11-01", "2022-11-02"]
+previous_date = compute_prev_date(list_of_dates)
+print(previous_date)
