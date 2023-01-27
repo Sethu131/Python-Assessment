3ac639998dbccdd1bba7bd54fa0bb7fdc99de8da
Sethu131 <123773376+Sethu131@users.noreply.github.com>
2023-01-27 22:32:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import re
-
-
-def is_date_format_correct(date: str) -> bool:
-    """
-    This function takes in a date in string format
-    and returns true if the date matches the format
-    YYYY-MM-DD and false if it doesn't
-    """
-
-    correct_format = re.compile(r'^\d{4}-\d{2}-\d{2}$')
-    return bool(correct_format.match(date))
-
-
-print(is_date_format_correct("1999-01-01"))
-print(is_date_format_correct("1999/01/01"))
+for i in range(1, 11):
+    if i == 6:
+        continue
+        print(i, end=',')
+    else:
+        print(i, end=',')
